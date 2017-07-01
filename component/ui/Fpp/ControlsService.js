2806a350e30241aab67a5adcdc3029e70cf9c98c
Javier escobar <cybersettler@babylonone.com>
2017-07-01 16:25:50 +0200

    changed ControlService

--- component/ui/Fpp/ControlsService.js
+++ component/ui/Fpp/ControlsService.js
@@ -3,7 +3,7 @@ const KeyboardControls = require( "./KeyboardControls.js" );
 module.exports = {
     initControls: function(perspective) {
         var mouseControls = perspective.view.shadowRoot.querySelector("ui-mousecontrols");
-        var closeUiBtn = perspective.view.querySelector(".closeUiBtn");
+        var closeUiBtn = perspective.view.querySelector("[role=close]");
         var keyboardControls = new KeyboardControls();
         keyboardControls.addDownEventHandler("escape", function(){
             mouseControls.dataset.lockpointer = false;
@@ -14,8 +14,8 @@ module.exports = {
                 closeUiBtn.onclick = function( e ){
                     e.preventDefault();
                     mouseControls.dataset.lockpointer = true;
-                    parentView.addEventListener('keydown', keyboardControls.registerKeyEvent, false);
-                    parentView.addEventListener('keyup', keyboardControls.registerKeyEvent, false);
+                    document.addEventListener('keydown', keyboardControls.registerKeyEvent, false);
+                    document.addEventListener('keyup', keyboardControls.registerKeyEvent, false);
                 };
             }
 
@@ -26,7 +26,8 @@ module.exports = {
             var args = e.detail;
             perspective.yawObject.rotation.y -= args.x;
             perspective.pitchObject.rotation.x -= args.y;
-            perspective.pitchObject.rotation.x = Math.max( - Math.PI/2, Math.min( Math.PI/2, view.model.pitchObject.rotation.x ) );
+            perspective.pitchObject.rotation.x = Math.max( - Math.PI/2, Math.min( Math.PI/2,
+                perspective.pitchObject.rotation.x ) );
 //  view.model.focus.set( viewModel.yawObject.position, view.getDirection() );
         }
     }
