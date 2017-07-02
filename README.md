fec694f3cb179e9de0f2197db1381b89a4cb9dc9
Javier escobar <cybersettler@babylonone.com>
2017-07-02 11:07:04 +0200

    added support for controlPanel role

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # uiFpp
 >First person perspective websemble component
 
+## API
+
+### data-scene
+
+Scene configuration data.
+
+### data-subject
+
+Subject refers to the point of view configuration, it
+may include not only camera information but also
+avatar data.
+
+## Supported roles
+
+### controlPanel
+
+Element containing controls to configure the perspective.
+
+### close
+
+Control that closes the overlay block element.
+
+
 
--- component/ui/Fpp/ControlsService.js
+++ component/ui/Fpp/ControlsService.js
@@ -3,22 +3,30 @@ const KeyboardControls = require( "./KeyboardControls.js" );
 module.exports = {
     initControls: function(perspective) {
         var mouseControls = perspective.view.shadowRoot.querySelector("ui-mousecontrols");
-        var closeUiBtn = perspective.view.querySelector("[role=close]");
+        var controlPanel = perspective.view.querySelector("[data-role=controlPanel]");
+        var closeUiBtn = perspective.view.querySelector("[data-role=close]");
         var keyboardControls = new KeyboardControls();
+
         keyboardControls.addDownEventHandler("escape", function(){
             mouseControls.dataset.lockpointer = false;
         });
-        perspective.keyboardControls = keyboardControls;
-        perspective.scope.getParentView().then(function(parentView) {
-            if(mouseControls && closeUiBtn){
-                closeUiBtn.onclick = function( e ){
-                    e.preventDefault();
-                    mouseControls.dataset.lockpointer = true;
-                    document.addEventListener('keydown', keyboardControls.registerKeyEvent, false);
-                    document.addEventListener('keyup', keyboardControls.registerKeyEvent, false);
-                };
-            }
 
+        perspective.keyboardControls = keyboardControls;
+
+        if (mouseControls && controlPanel) {
+            controlPanel.addEventListener("click", function(e) {
+                var closeBtn = e.path.find(function(item) {
+                    return item.dataset && item.dataset.role && item.dataset.role === "close";
+                });
+                if (closeBtn) {
+                    lockPointer(e);
+                }
+            });
+        }
+        if(mouseControls && closeUiBtn){
+            closeUiBtn.onclick = lockPointer;
+        }
+        perspective.scope.getParentView().then(function(parentView) {
             parentView.addEventListener("mouseMove", rotatePerspective);
         });
 
@@ -29,6 +37,13 @@ module.exports = {
             perspective.pitchObject.rotation.x = Math.max( - Math.PI/2, Math.min( Math.PI/2,
                 perspective.pitchObject.rotation.x ) );
 //  view.model.focus.set( viewModel.yawObject.position, view.getDirection() );
+        }
+
+        function lockPointer(e){
+            e.preventDefault();
+            mouseControls.dataset.lockpointer = true;
+            document.addEventListener('keydown', keyboardControls.registerKeyEvent, false);
+            document.addEventListener('keyup', keyboardControls.registerKeyEvent, false);
         }
     }
 };
